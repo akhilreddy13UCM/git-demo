e4f645cf0cf77d57e3aaec6743ed6829c91c9cbb
akhilreddy13UCM <axe55580@ucmo.edu>
2024-11-06 20:29:04 -0600

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world2!");
+        System.out.println("Hello world3!");
 //        git status
-//        git init -b main
+//        git init -b main   [create main branch initializing git]
 //        git status
-//        git add src
+//        git add src       [moving from working directory to staging area]
 //        git log
-//        git commit -m "first commit"
+//        git commit -m "first commit"   [moving from staging to local commit in local repo]
 
     }
 }
