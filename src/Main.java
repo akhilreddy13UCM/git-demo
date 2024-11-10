ce649e4948d519152e351444e9c4a48fbbae27ce
akhilreddy13UCM <axe55580@ucmo.edu>
2024-11-10 12:34:40 -0600

    feature1 commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world4!");
         int a=20;
-        System.out.println(a);
+        System.out.println(a+"feature1");
 //        git status
 //        git init -b main   [create main branch initializing git]
 //        git status
@@ -10,6 +10,18 @@ public class Main {
 //        git log
 //        git commit -m "first commit"   [moving from staging to local commit in local repo]
 //        git commit -a -m "third commit"  [moving from working directory to local commit]
-
+//        git diff
+//        git add src
+//        git diff --staged    [can know code differences in staging area]
+//        Now from local repo to remote repo
+//        git remote add origin https://github.com/akhilreddy13UCM/git-demo.git
+//        git push -u origin main
+//        git remote -v  [gives output as origin(remote repository name is origin,but the repository itself is named git-demo]
+//        git tag v1 -m "Nov 8th"
+//        git tag
+//        git push -u origin v1
+//        git show v1
+//        git checkout -b feature1
+//        git branch
     }
 }
